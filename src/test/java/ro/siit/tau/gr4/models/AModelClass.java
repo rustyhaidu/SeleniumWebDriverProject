a05fc897fbed12aa49208c9daabba01a4cc0fba0
Adi Moldovan <adim.moldovan@gmail.com>
2017-08-29 10:36:50 +0300

    Initial commit

---
+++ src/test/java/ro/siit/tau/gr4/models/AModelClass.java
@@ -0,0 +1,5 @@
+package ro.siit.tau.gr4.models;
+
+public class AModelClass {
+    // YOU CAN DELETE ME. IF NOT, YOU SHOULD DEFINITELY RENAME ME
+}
---
+++ src/test/java/ro/siit/tau/gr4/tests/ATestClass.java
@@ -0,0 +1,5 @@
+package ro.siit.tau.gr4.tests;
+
+public class ATestClass {
+    // YOU CAN DELETE ME. IF NOT, YOU SHOULD DEFINITELY RENAME ME
+}
