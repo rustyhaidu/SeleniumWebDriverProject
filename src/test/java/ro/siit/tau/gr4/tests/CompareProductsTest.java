8bad8d09d722db29214c4c734d98833674292b9f
Claudiu <haidu_claudiu@yahoo.com>
2017-09-07 21:18:25 +0300

    get text from cart button

--- src/test/java/ro/siit/tau/gr4/tests/CompareProductsTest.java
+++ src/test/java/ro/siit/tau/gr4/tests/CompareProductsTest.java
@@ -1,14 +1,30 @@
 package ro.siit.tau.gr4.tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ro.siit.tau.gr4.pages.HomePage;
+
+import java.util.List;
 
 public class CompareProductsTest extends BaseTest{
 
     private HomePage homePage;
 
+    @BeforeMethod
+    public void setCompareTest() {
+        homePage = PageFactory.initElements(driver, HomePage.class);
+    }
+
     @Test
     public void compareItemsTest(){
         homePage.searchItem(" ");
+
+        List<WebElement> listOfProducts = driver.findElements(By.xpath("//div[@class='product-thumb']"));
+        WebElement addToCompareButton = listOfProducts.get(1).findElement(By.cssSelector("button[data-original-title='Compare this Product']"));
+        addToCompareButton.click();
+
     }
 }
