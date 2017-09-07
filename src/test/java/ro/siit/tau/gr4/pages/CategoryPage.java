328b2121041d3a4a8900f54ae92c3fe59c0c5e23
Claudiu <haidu_claudiu@yahoo.com>
2017-09-07 19:17:37 +0300

    deleted imports

--- src/test/java/ro/siit/tau/gr4/pages/CategoryPage.java
+++ src/test/java/ro/siit/tau/gr4/pages/CategoryPage.java
@@ -1,14 +1,12 @@
 package ro.siit.tau.gr4.pages;
 
 
-import com.gargoylesoftware.htmlunit.javascript.host.Touch;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class CategoryPage {
 
