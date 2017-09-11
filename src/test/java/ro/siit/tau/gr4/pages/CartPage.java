7eee896d88f654f9e5bfa9723bdf295818c95ed2
pureluckystrike <liviabd84@gmail.com>
2017-09-11 23:53:46 +0300

    cleanup

--- src/test/java/ro/siit/tau/gr4/pages/CartPage.java
+++ src/test/java/ro/siit/tau/gr4/pages/CartPage.java
@@ -9,9 +9,6 @@ import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.Iterator;
-import java.util.List;
-
 public class CartPage {
 
     WebDriver driver;
@@ -59,18 +56,6 @@ public class CartPage {
         checkoutButton.click();
     }
 
-  /*  public WebElement getQuantityField(String product) {
-        WebElement productInCart = driver.findElement(By.xpath("//tr[descendant::a[text()='" +
-            product + "']]//td[4]//input[@type='text']"));
-        return productInCart;
-    }
-
-   /* public WebElement getCartRow(String product){
-        String descendent = "//form/div[@class='table-responsive']/table[@class='table table-bordered']/tbody/tr[descendant::a[text()='" + product + "']]";
-        return (new WebDriverWait( driver, 3))
-            .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(descendent)));
-    }*/
-
    public WebElement getCartRow(String product){
         return (new WebDriverWait( driver, 3))
             .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@class='table table-bordered']/tbody/tr[descendant::a[text()='"
@@ -82,27 +67,10 @@ public class CartPage {
         return cartRow.findElement(By.xpath(".//input[@type='text' and contains(@name,'quantity')]"));
     }
 
-    /*public WebElement getQuantityField(String product){
-        WebElement cartRow = this.getCartRow(product);
-        return cartRow.findElement(By.cssSelector("input[name^='quantity']"));
-    }*/
-
     public String readQuantity(String product){
         WebElement quantityField = this.getQuantityField(product);
         return quantityField.getAttribute("value");
     }
 
-    public void clickLinkByHref(String href) {
-        List<WebElement> anchors = driver.findElements(By.tagName("a"));
-        Iterator<WebElement> i = anchors.iterator();
-
-        while (i.hasNext()) {
-            WebElement anchor = i.next();
-            if (anchor.getAttribute("href").contains(href)) {
-                anchor.click();
-                break;
-            }
-        }
-    }
 
 }
