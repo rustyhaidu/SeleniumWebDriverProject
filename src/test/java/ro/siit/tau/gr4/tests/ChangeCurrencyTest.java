c955b03ba0ae0eaff10fee9d871ed77f09579938
pureluckystrike <liviabd84@gmail.com>
2017-09-13 21:01:00 +0300

    ch currency test

--- src/test/java/ro/siit/tau/gr4/tests/ChangeCurrencyTest.java
+++ src/test/java/ro/siit/tau/gr4/tests/ChangeCurrencyTest.java
@@ -1,6 +1,28 @@
 package ro.siit.tau.gr4.tests;
 
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ro.siit.tau.gr4.pages.HomePage;
+
 public class ChangeCurrencyTest extends BaseTest {
 
+    @Test
+    public void CurrencyTest(){
+
+            HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+
+            homePage.clickCurrencyButton();
+            homePage.setCurrency("GBP", driver);
+
+        Assert.assertEquals(homePage.readCurrencySymbol(),
+            "£",
+            "check currency symbol is correct");
+
+        Assert.assertEquals(homePage.getTextFromCartTotalButton().contains("£"),
+            true,
+            "check ");
+
+        }
 
 }
