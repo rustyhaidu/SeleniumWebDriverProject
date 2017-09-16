7a5ae50c2ee3e0970319f020b12456d085781421
Claudiu <haidu_claudiu@yahoo.com>
2017-09-16 15:15:19 +0300

    fixed for cmd a little

--- src/test/java/ro/siit/tau/gr4/tests/ChangeCurrencyTest.java
+++ src/test/java/ro/siit/tau/gr4/tests/ChangeCurrencyTest.java
@@ -23,22 +23,26 @@ public class ChangeCurrencyTest extends BaseTest {
         homePage.clickCurrencyButton();
         homePage.setCurrency("GBP", driver);
         Assert.assertEquals(homePage.readCurrencySymbol(),
-            "£",
+            //"£",
+            "\u00A3",
             "check currency symbol is correctly set in the currency dropdown button");
-        Assert.assertEquals(homePage.getTextFromCartTotalButton().contains("£"),
+        Assert.assertEquals(homePage.getTextFromCartTotalButton().contains("\u00A3"),
             true,
             "check currency symbol is displayed on the Cart Button");
         Assert.assertEquals(homePage.getProductCurrencySymbol("MacBook"),
-            "££",
+            //"££",
+            "\u00A3\u00A3",
             "check currency symbol is displayed twice: in price and in Ex.Tax Price");
 
         //go to the product page
         categoryPage.clickProduct("iPhone", driver);
         Assert.assertEquals(productPage.getCurrencySymbolFromPrice(),
-            "£",
+            //"£",
+            "\u00A3",
             "check currency symbol is displayed in price");
         Assert.assertEquals(productPage.getCurrencySymbolFromExTaxPrice(),
-            "£",
+            //"£",
+            "\u00A3",
             "check currency symbol is displayed in Ex.Tax Price");
 
     }
