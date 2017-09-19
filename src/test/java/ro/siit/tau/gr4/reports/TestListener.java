4cf214c41fbb1c88b032f53cb66abe9241c71ffb
Claudiu <haidu_claudiu@yahoo.com>
2017-09-19 23:58:34 +0300

    Reports

--- src/test/java/ro/siit/tau/gr4/reports/TestListener.java
+++ src/test/java/ro/siit/tau/gr4/reports/TestListener.java
@@ -1,17 +1,30 @@
 package ro.siit.tau.gr4.reports;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.LogStatus;
+import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
-import java.lang.reflect.Method;
 
 public class TestListener extends TestListenerAdapter {
+    private ExtentReports extentReports = new ExtentReports();
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+        super.onStart(iTestContext);
+        extentReports.init("extent-report.html",true);
+    }
+
 
     public void onTestStart(ITestResult result) {
         String testMethodName = result.getMethod().getMethodName();
         String testDescription = result.getMethod().getDescription();
         System.out.println();
         System.out.println("START TEST " + testMethodName);
+
+        //extentReports = ExtentReports.get(result.getTestClass().getRealClass());
+        extentReports.startTest(testMethodName);
         if (testDescription != null) {
             if (!testDescription.isEmpty()) {
                 System.out.println("Description: " + testDescription);
@@ -21,16 +34,32 @@ public class TestListener extends TestListenerAdapter {
 
     @Override
     public void onTestSuccess(ITestResult result){
-
+        System.out.println("#####Start Test Custom Message for Success#####");
+        String testMethodName = result.getMethod().getMethodName();
+        extentReports.log(LogStatus.PASS,testMethodName);
+        extentReports.endTest();
     }
 
     @Override
     public void onTestFailure(ITestResult result){
         System.out.println("#####Start Test Custom Message for Failure#####");
+        String testMethodName = result.getMethod().getMethodName();
+        extentReports.log(LogStatus.FAIL,testMethodName);
+        extentReports.endTest();
     }
 
     @Override
     public void onTestSkipped(ITestResult result){
         System.out.println("#####Start Test Custom Message for SKIPPY#####");
+        String testMethodName = result.getMethod().getMethodName();
+        extentReports.log(LogStatus.SKIP,testMethodName);
+        extentReports.endTest();
+    }
+
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+        super.onFinish(iTestContext);
+
     }
 }
--- src/test/java/ro/siit/tau/gr4/reports/TestReporter.java
+++ src/test/java/ro/siit/tau/gr4/reports/TestReporter.java
@@ -1,34 +1,61 @@
 package ro.siit.tau.gr4.reports;
 
-import org.testng.IReporter;
-import org.testng.ISuite;
+import com.relevantcodes.extentreports.ExtentReports;
+import org.testng.*;
 import org.testng.xml.XmlSuite;
 
 import java.io.*;
 import java.util.List;
+import java.util.Map;
 
 
 public class TestReporter implements IReporter {
 
-    public void generateReport(List<XmlSuite> xml, List<ISuite> suites, String outDir)
+    public void generateReport(List<XmlSuite> xml, List<ISuite> suites, String outDir) {
 
-    {
-        PrintWriter reportWriter = null;
-        File targetFile = new File("G:\\Scoala Informala Testare Automata\\Projects\\ShopProject1\\siit-gr4-team1\\report.html");
-        try {
-            reportWriter = new PrintWriter(new BufferedWriter(new FileWriter(targetFile)));
-        } catch (IOException e) {
-            e.printStackTrace();
+        int passed = 0, failed = 0, skipped = 0, total = 0;/* Iterate over each suite */
+        for (ISuite suite : suites) {   /* Get all results from the current suite */
+            Map<String, ISuiteResult> suiteResults = suite.getResults();
+            for (ISuiteResult sr : suiteResults.values()) {
+                ITestContext tc = sr.getTestContext();      /* Get all test methods */
+                for (ITestNGMethod n : tc.getAllTestMethods()) {
+                    total++;
+                }      /* Get all passed test scenarios */
+                for (ITestResult r : tc.getPassedTests().getAllResults()) {
+                    passed++;
+                }      /* Get all failed test scenarios */
+                for (ITestResult r : tc.getFailedTests().getAllResults()) {
+                    failed++;
+                }      /* Get all skipped test scenarios */
+                for (ITestResult r : tc.getSkippedTests().getAllResults()) {
+                    skipped++;
+                }
+            }
+
+
+            PrintWriter reportWriter = null;
+            File targetFile = new File("basicReport.html");
+            try {
+                reportWriter = new PrintWriter(new BufferedWriter(new FileWriter(targetFile)));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            reportWriter.println("<!DOCTYPE html>");
+            reportWriter.println("<html><head><title>Test Run Report</title></head>");
+            reportWriter.println("<body>");
+            reportWriter.println("<p>This is my custom test report</p>");
+            reportWriter.println("<p>PASSED:  "+passed+"</p>");
+            reportWriter.println("<p>FAILED: "+failed+"</p>");
+            reportWriter.println("<p>SKIPPED: "+skipped+"</p>");
+            reportWriter.println("</body>");
+            reportWriter.println("</html>");
+            reportWriter.flush();
+            reportWriter.close();
+
+            reportWriter.flush();
+            reportWriter.close();
         }
-        reportWriter.println("<!DOCTYPE html>");
-        reportWriter.println("<html><head><title>Test Run Report</title></head>");
-        reportWriter.println("<body>");
-        reportWriter.println("<p>This is my custom test report</p>");
-        reportWriter.println("</body>");
-        reportWriter.println("</html>");
-        reportWriter.flush();
-        reportWriter.close();
+
     }
 }
-
 
